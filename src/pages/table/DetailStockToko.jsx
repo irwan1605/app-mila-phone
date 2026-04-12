3daf7f8100b79234d672ed46f994dcffa99b2f39
irwan1605 <muhamadirwan1605@gmail.com>
2026-04-12 16:21:29 +0700

    del

--- src/pages/table/DetailStockToko.jsx
+++ src/pages/table/DetailStockToko.jsx
@@ -572,13 +572,13 @@ export default function DetailStockToko() {
                     <td className="px-3 py-2 text-center">
                       <div className="flex gap-2 justify-center">
                         {/* ✅ DELETE */}
-                        <button
+                        {/* <button
                           onClick={() => handleDelete(r)}
                           title="Hapus Permanen"
                           className="bg-red-600 hover:bg-red-700 text-white px-2 py-1 rounded text-xs"
                         >
                           🗑️
-                        </button>
+                        </button> */}
 
                         {/* ✅ TRANSFER */}
                         <button
